c3fc23c56e4caee6cab13ba69f7d6de0166a6c16
dinhvanvu94 <dinhvanvu94@gmail.com>
2022-08-27 18:33:00 +0700

    Kafka Fix

--- example/main.go
+++ example/main.go
@@ -4,23 +4,29 @@ import (
 	"context"
 	"fmt"
 	_ "github.com/briansamuel/asynjob"
-	"github.com/briansamuel/kafpubsub"
-	"log"
-	"strings"
+	kafka "github.com/briansamuel/kafpubsub"
+	"github.com/briansamuel/kafpubsub/kafkapb"
+
 	"time"
 )
 
 func main() {
 
 	var brokerURL = "0.0.0.0:9092"
-	brokers := strings.Split(brokerURL, ",")
-	client := kafpubsub.NewClient(brokers)
-	kafps := kafpubsub.NewKafkaPubSub(client)
+
+	kafps := kafkapb.NewKafkaPubSub(brokerURL)
 	appCtx := NewAppContext(kafps)
-	subscriber := kafpubsub.NewSubscriber(appCtx)
-	NewEngine(subscriber).Start()
-	kafps.Publish(context.Background(), "topic-4", nil)
-	kafps.Publish(context.Background(), "topic-5", nil)
+	var subcriber kafka.Subscriber
+	subcriber = kafka.NewSubscriber(appCtx)
+
+	subcriber.StartSubTopic(
+		"topic-1",
+		true,
+		Topic1HandleFunction(appCtx),
+	)
+	subcriber.Start()
+	_ = kafps.Publish(context.Background(), "topic-1", nil)
+	_ = kafps.Publish(context.Background(), "topic-2", nil)
 
 	// Calling Sleep method
 	time.Sleep(60 * time.Second)
@@ -29,59 +35,10 @@ func main() {
 	fmt.Println("Sleep Over.....")
 }
 
-type Engine struct {
-	sub kafpubsub.Subscriber
-}
-
-func NewEngine(sub kafpubsub.Subscriber) *Engine {
-	return &Engine{sub: sub}
-}
-
-func (sb *Engine) GetSub() kafpubsub.Subscriber { return sb.sub }
-
-func (sb *Engine) Start() error {
-
-	sb.Setup()
-	return nil
-}
-
-func (sb *Engine) Setup() {
-
-	sb.GetSub().StartSubTopic(
-		"topic-4",
-		true,
-		Topic1HandleFunction(sb.GetSub().GetAppCtx()),
-	)
-
-	// Follow topic in queue
-	sb.GetSub().StartSubTopic(
-		"topic-5",
-		true,
-		Topic2HandleFunction(sb.GetSub().GetAppCtx()),
-	)
-
-	err := sb.GetSub().InitialClient()
-	if err != nil {
-		log.Print(err)
-	}
-
-}
-
-func Topic1HandleFunction(appCtx kafpubsub.AppContext) kafpubsub.ConsumerJob {
-	return kafpubsub.ConsumerJob{
+func Topic1HandleFunction(appCtx *appCtx) kafka.ConsumerJob {
+	return kafka.ConsumerJob{
 		Title: "Topic1HandleFunction",
-		Hdl: func(ctx context.Context, msg *kafpubsub.Message) error {
-
-			fmt.Sprintf("Message %s of topic %s", msg.Data(), msg.Chanel())
-			return nil
-		},
-	}
-}
-
-func Topic2HandleFunction(appCtx kafpubsub.AppContext) kafpubsub.ConsumerJob {
-	return kafpubsub.ConsumerJob{
-		Title: "Topic2HandleFunction",
-		Hdl: func(ctx context.Context, msg *kafpubsub.Message) error {
+		Hdl: func(ctx context.Context, msg *kafka.Message) error {
 
 			fmt.Sprintf("Message %s of topic %s", msg.Data(), msg.Chanel())
 			return nil
@@ -90,11 +47,11 @@ func Topic2HandleFunction(appCtx kafpubsub.AppContext) kafpubsub.ConsumerJob {
 }
 
 type appCtx struct {
-	kafkaPs kafpubsub.PubSub
+	kafkaPs kafka.PubSub
 }
 
-func NewAppContext(kafkaPs kafpubsub.PubSub) *appCtx {
+func NewAppContext(kafkaPs kafka.PubSub) *appCtx {
 	return &appCtx{kafkaPs: kafkaPs}
 }
 
-func (ctx *appCtx) GetKafka() kafpubsub.PubSub { return ctx.kafkaPs }
+func (ctx *appCtx) GetKafka() kafka.PubSub { return ctx.kafkaPs }
